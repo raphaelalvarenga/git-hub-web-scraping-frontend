1b283b9f0be7f06470790b2822da84f27cbf1c20
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2020-06-14 14:34:31 -0300

    [Icon and Search Bar] Created a Grid container with two items: Icon and Search Bar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import * as React from "react";
-import { Button, CssBaseline } from "@material-ui/core";
+import { Button, CssBaseline, Grid, TextField } from "@material-ui/core";
+import { GitHub, Search } from "@material-ui/icons";
 
 const App: React.FunctionComponent = () => (
     <CssBaseline>
-        <h1>Application</h1>
-        <Button variant = "contained" color = "primary">Botão</Button>
+        <Grid container spacing = {3} style = {{maxWidth: "1200px", margin: "auto", width: "100%"}}>
+            <Grid item xs = {12} style = {{textAlign: "center"}}>
+                <GitHub color = "primary" style = {{fontSize: 80}} />
+            </Grid>
+
+            <Grid item xs = {12} style = {{textAlign: "center"}}>
+                <TextField label = "Insert a GitHub repository link" style = {{minWidth: "600px"}} />
+            </Grid>
+
+            <Grid item xs = {12} style = {{textAlign: "center"}}>
+                <Button color = "primary" variant = "outlined"><Search />Search...</Button>
+            </Grid>
+
+            
+        </Grid>
     </CssBaseline>
 )
 
